fbfe2eb30cd8f4aeb0fa7eebb58a7d134c302ba4
Jasper Shovelton <Beanie.github@proton.me>
2023-10-16 10:52:39 +0100

    Add a method for pretty-printing the board.

--- src/board.rs
+++ src/board.rs
@@ -24,4 +24,63 @@ impl Board {
             kings:   0x1000000000000010,
         }
     }
+
+    pub fn pretty_print(&self) {
+        for r in (Ranks::RANK1..=Ranks::RANK8).rev() {
+            print!("{} | ", rank_to_char(r));
+            for f in Files::FILE1..=Files::FILE8 {
+                print!("{} ", self.char_piece_from_pos(r, f));
+            }
+            println!("")
+        }
+        println!("    ---------------");
+        println!("    1 2 3 4 5 6 7 8");
+    }
+
+    fn char_piece_from_pos(&self, rank: u8, file: u8) -> char {
+        let sq_bb = bitboard_from_pos(rank, file);
+        if sq_bb & self.white != 0 {
+            if sq_bb & self.pawns != 0 {
+                'P'
+            } else if sq_bb & self.knights != 0 {
+                'N'
+            } else if sq_bb & self.bishops != 0 {
+                'B'
+            } else if sq_bb & self.rooks != 0 {
+                'R'
+            } else if sq_bb & self.queens != 0 {
+                'Q'
+            } else if sq_bb & self.kings != 0 {
+                'K'
+            } else {
+                panic!("White bb does not match up with the piece bbs");
+            }
+        } else if sq_bb & self.black != 0 {
+            if sq_bb & self.pawns != 0 {
+                'p'
+            } else if sq_bb & self.knights != 0 {
+                'n'
+            } else if sq_bb & self.bishops != 0 {
+                'b'
+            } else if sq_bb & self.rooks != 0 {
+                'r'
+            } else if sq_bb & self.queens != 0 {
+                'q'
+            } else if sq_bb & self.kings != 0 {
+                'k'
+            } else {
+                panic!("Black bb does not match up with the piece bbs");
+            }
+        } else {
+            '0'
+        }
+    }
+}
+
+fn rank_to_char(rank: u8) -> char {
+    char::from_u32('A' as u32 + rank as u32).unwrap()
+}
+
+fn bitboard_from_pos(rank: u8, file: u8) -> Bitboard {
+    1u64 << (rank * 8 + file)
 }
