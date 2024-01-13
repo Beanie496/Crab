aa6ea69415a3aa489e9638b4334123dd571ed7b0
Jasper Shovelton <Beanie.github@proton.me>
2024-01-13 02:39:02 +0000

    Make private all unnecessary methods of `Engine`.

--- src/engine.rs
+++ src/engine.rs
@@ -27,54 +27,33 @@ impl Engine {
 }
 
 impl Engine {
-    /// Clears the board.
-    pub fn clear_board(&mut self) {
-        self.board.clear_board();
-    }
-
-    /// Takes a sequence of moves and feeds them to the board. Will stop and
-    /// return if any of the moves are incorrect. Not implemented yet.
-    pub fn play_moves(&self, _moves: &str) {}
-
     /// Pretty-prints the current state of the board.
     pub fn pretty_print_board(&self) {
         self.board.pretty_print();
     }
 
-    /// Adds the given right to the castling rights of the board.
-    pub fn add_castling_right(&mut self, right: CastlingRights) {
-        self.board.add_castling_right(right);
+    /// Sets `self.board` to the given FEN and moves, as given by the
+    /// `position` UCI command. Unexpected/incorrect tokens will be ignored.
+    pub fn set_position(&mut self, position: &str, moves: &str) {
+        self.set_pos_to_fen(position);
+        self.play_moves(moves);
     }
 
-    /// Sets the default castling right/s `right`.
-    pub fn set_default_castling_rights(&mut self) {
-        self.board.set_default_castling_rights();
+    /// Resets `self.board`.
+    pub fn set_startpos(&mut self) {
+        self.board.set_startpos();
     }
+}
 
-    /// Sets default side to move.
-    pub fn set_default_side_to_move(&mut self) {
-        self.board.set_default_side_to_move();
-    }
-
-    /// Sets the ep square.
-    pub fn set_ep_square(&mut self, square: Square) {
-        self.board.set_ep_square(square);
-    }
-
-    /// Sets the fullmove counter.
-    pub fn set_fullmoves(&mut self, count: u32) {
-        self.board.set_fullmoves(count);
-    }
-
-    /// Sets the halfmove counter.
-    pub fn set_halfmoves(&mut self, count: u32) {
-        self.board.set_halfmoves(count);
-    }
+impl Engine {
+    /// Takes a sequence of moves and feeds them to the board. Will stop and
+    /// return if any of the moves are incorrect. Not implemented yet.
+    fn play_moves(&self, _moves: &str) {}
 
     /// Sets `self.board` to the given FEN. It will check for basic errors,
     /// like the board having too many ranks, but not many more.
-    pub fn set_pos_to_fen(&mut self, position: &str) {
-        self.clear_board();
+    fn set_pos_to_fen(&mut self, position: &str) {
+        self.board.clear_board();
 
         let mut iter = position.split(' ');
         let board = if let Some(x) = iter.next() {
@@ -140,9 +119,9 @@ impl Engine {
         // 2. side to move
         if let Some(stm) = side_to_move {
             if stm == "w" {
-                self.set_side_to_move(Side::WHITE);
+                self.board.set_side_to_move(Side::WHITE);
             } else if stm == "b" {
-                self.set_side_to_move(Side::BLACK);
+                self.board.set_side_to_move(Side::BLACK);
             } else {
                 self.set_startpos();
                 return eprintln!("Error: Side to move \"{stm}\" is not \"w\" or \"b\"");
@@ -151,17 +130,17 @@ impl Engine {
             // I've decided that everything apart from the board can be omitted
             // and guessed, so if there's nothing given, default to White to
             // move.
-            self.set_default_side_to_move();
+            self.board.set_default_side_to_move();
         }
 
         // 3. castling rights
         if let Some(cr) = castling_rights {
             for right in cr.chars() {
                 match right {
-                    'K' => self.add_castling_right(CastlingRights::CASTLE_FLAGS_K),
-                    'Q' => self.add_castling_right(CastlingRights::CASTLE_FLAGS_Q),
-                    'k' => self.add_castling_right(CastlingRights::CASTLE_FLAGS_k),
-                    'q' => self.add_castling_right(CastlingRights::CASTLE_FLAGS_q),
+                    'K' => self.board.add_castling_right(CastlingRights::CASTLE_FLAGS_K),
+                    'Q' => self.board.add_castling_right(CastlingRights::CASTLE_FLAGS_Q),
+                    'k' => self.board.add_castling_right(CastlingRights::CASTLE_FLAGS_k),
+                    'q' => self.board.add_castling_right(CastlingRights::CASTLE_FLAGS_q),
                     '-' => (),
                     _ => {
                         self.set_startpos();
@@ -171,11 +150,11 @@ impl Engine {
             }
         } else {
             // KQkq if nothing is given.
-            self.set_default_castling_rights();
+            self.board.set_default_castling_rights();
         }
 
         // 4. en passant
-        self.set_ep_square(if let Some(ep) = ep_square {
+        self.board.set_ep_square(if let Some(ep) = ep_square {
             if ep == "-" {
                 Square::NONE
             } else if let Some(square) = Square::from_string(ep) {
@@ -189,7 +168,7 @@ impl Engine {
         });
 
         // 5. halfmoves
-        self.set_halfmoves(if let Some(hm) = halfmoves {
+        self.board.set_halfmoves(if let Some(hm) = halfmoves {
             if let Ok(hm) = hm.parse::<u32>() {
                 hm
             } else {
@@ -201,7 +180,7 @@ impl Engine {
         });
 
         // 6. fullmoves
-        self.set_fullmoves(if let Some(fm) = fullmoves {
+        self.board.set_fullmoves(if let Some(fm) = fullmoves {
             if let Ok(fm) = fm.parse::<u32>() {
                 fm
             } else {
@@ -210,22 +189,5 @@ impl Engine {
         } else {
             0
         });
-    }
-
-    /// Sets `self.board` to the given FEN and moves, as given by the
-    /// `position` UCI command. Unexpected/incorrect tokens will be ignored.
-    pub fn set_position(&mut self, position: &str, moves: &str) {
-        self.set_pos_to_fen(position);
-        self.play_moves(moves);
-    }
-
-    /// Sets side to move to `side`.
-    pub fn set_side_to_move(&mut self, side: Side) {
-        self.board.set_side_to_move(side);
-    }
-
-    /// Resets `self.board`.
-    pub fn set_startpos(&mut self) {
-        self.board.set_startpos();
     }
 }
