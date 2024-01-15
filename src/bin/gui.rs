194a81ef1f86bf3b8d240645074e0f71d3b76824
Jasper Shovelton <Beanie.github@proton.me>
2024-01-15 14:56:36 +0000

    Set up the initial layout.
    The board area is coloured white and the info box is coloured red for my
    own benefit.

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -1,10 +1,34 @@
-use eframe::{egui, run_simple_native, Error, NativeOptions};
+use eframe::{
+    egui::{self, Color32, Frame, Id, Vec2, ViewportBuilder},
+    run_simple_native, Error, NativeOptions,
+};
 
 fn main() -> Result<(), Error> {
     let title = "Crab - A chess engine";
 
-    let options = NativeOptions::default();
+    let options = NativeOptions {
+        viewport: ViewportBuilder::default()
+            .with_title(title)
+            .with_decorations(false)
+            .with_inner_size(Vec2::new(1920.0, 1080.0)),
+        ..Default::default()
+    };
+
     run_simple_native(title, options, move |ctx, _frame| {
-        egui::CentralPanel::default().show(ctx, |_ui| {});
+        let ppp = ctx.native_pixels_per_point().unwrap();
+        let board_area_width = 880.0 / ppp;
+        let info_box_width = (1920.0 - 880.0) / ppp;
+        egui::SidePanel::left(Id::new(0))
+            .resizable(false)
+            .show_separator_line(false)
+            .exact_width(board_area_width)
+            .frame(Frame::none().fill(Color32::WHITE))
+            .show(ctx, |_ui| {});
+        egui::SidePanel::right(Id::new(1))
+            .resizable(false)
+            .show_separator_line(false)
+            .exact_width(info_box_width)
+            .frame(Frame::none().fill(Color32::RED))
+            .show(ctx, |_ui| {});
     })
 }
