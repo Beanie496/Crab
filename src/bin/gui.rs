35741e205b8f4a92f16a20a8fc66e675b8590f3c
Jasper Shovelton <Beanie.github@proton.me>
2024-01-15 12:21:12 +0000

    Implement the initial window.

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use eframe::{egui, run_simple_native, Error, NativeOptions};
+
+fn main() -> Result<(), Error> {
+    let title = "Crab - A chess engine";
+
+    let options = NativeOptions::default();
+    run_simple_native(title, options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(ctx, |_ui| {});
+    })
+}
