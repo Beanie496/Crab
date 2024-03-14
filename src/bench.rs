f948e79fbcb1b0313f7e991d17b0da36cb1e8084
Jasper Shovelton <Beanie.github@proton.me>
2024-03-14 13:37:10 +0000

    Make the test code use positions from a file instead.
    This allows new positions to be tested easily.
    (And yes, all the positions passed.)

--- src/bench.rs
+++ src/bench.rs
@@ -7,63 +7,16 @@ use std::{
 
 use crate::engine::Engine;
 
-use lazy_static::lazy_static;
-
-#[derive(Clone, Copy)]
-struct TestPosition<'a> {
-    position: &'a str,
+struct TestPosition {
+    position: String,
     perft_depth: u8,
     perft_result: u64,
 }
 
-lazy_static! {
-    /// Test positions used to check the correctness of movegen/make/unmake.
-    static ref TEST_POSITIONS: Vec<TestPosition<'static>> = vec![
-        // startpos. Depth 6 has ep, checks, discovered checks, double checks
-        // and checkmates.
-        TestPosition::new(
-            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-            6,
-            119_060_324,
-        ),
-        // kiwipete. Depth 4 tests everything, but depth 5 to be safe.
-        TestPosition::new(
-            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-            5,
-            193_690_690,
-        ),
-        // tests ep which would be a discovered attack on own king
-        TestPosition::new(
-            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -",
-            5,
-            674_624
-        ),
-        TestPosition::new(
-            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-            4,
-            422_333,
-        ),
-        TestPosition::new(
-            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-            5,
-            15_833_292,
-        ),
-        // tests enemy knight taking own rook, disallowing castling
-        TestPosition::new(
-            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 0 1",
-            5,
-            89_941_194,
-        ),
-        TestPosition::new(
-            "r3k2r/2pb1ppp/2pp1q2/p7/1nP1B3/1P2P3/P2N1PPP/R2QK2R w KQkq a6 0 1",
-            5,
-            67_956_855,
-        ),
-    ];
-}
+static TEST_POSITIONS: &str = include_str!("../test_positions.epd");
 
-impl<'a> TestPosition<'a> {
-    const fn new(position: &'a str, perft_depth: u8, perft_result: u64) -> Self {
+impl TestPosition {
+    const fn new(position: String, perft_depth: u8, perft_result: u64) -> Self {
         Self {
             position,
             perft_depth,
@@ -72,9 +25,9 @@ impl<'a> TestPosition<'a> {
     }
 }
 
-impl TestPosition<'_> {
+impl TestPosition {
     fn run_test(&self, engine: &mut Engine) {
-        engine.set_position(self.position, "");
+        engine.set_position(&self.position, "");
         println!("Position: {}", self.position);
         assert_eq!(
             engine.perft::<false, false>(self.perft_depth),
@@ -91,8 +44,26 @@ fn test_positions() {
     let mut handles = Vec::new();
 
     // add all test positions to the queue
-    for position in TEST_POSITIONS.iter() {
-        tx.send(position).unwrap();
+    for position in TEST_POSITIONS.lines() {
+        let mut tokens = position.split(' ');
+
+        let result = tokens
+            .next_back()
+            .and_then(|result| result.parse::<u64>().ok())
+            .unwrap();
+
+        let mut fen = String::new();
+        for token in tokens.take(6) {
+            fen.push_str(&token);
+            fen.push(' ');
+        }
+        fen.pop();
+
+        // each position is just to depth 4
+        let depth = 4;
+
+        let test_pos = TestPosition::new(fen, depth, result);
+        tx.send(test_pos).unwrap();
     }
 
     // create as many threads as is optimal. If no threads available, the test
