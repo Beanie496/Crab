48204bbab66471c642895ef43ce6f1a230dec284
June Shovelton <Beanie.github@proton.me>
2026-05-03 15:58:35 +0100

    Update copyright years.
    Bench: 12386906

--- src/search/movepick.rs
+++ src/search/movepick.rs
@@ -1,6 +1,6 @@
 /*
  * Crab, a UCI-compatible chess engine
- * Copyright (C) 2024 June Shovelton
+ * Copyright (C) 2026 June Shovelton
  *
  * Crab is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
